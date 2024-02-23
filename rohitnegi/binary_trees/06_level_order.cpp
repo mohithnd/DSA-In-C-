d497a2d0fe1b7c9517a5c515a80426bba19f3072
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-02-23 14:06:28 +0530

    Level order traversal of a tree

---
+++ rohitnegi/binary_trees/06_level_order.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
+        this->data = data;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+};
+
+void level_order(Node *root)
+{
+    if (root == nullptr)
+    {
+        cout << "Tree Is Empty" << endl;
+        return;
+    }
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+        cout << temp->data << " ";
+        if (temp->left)
+        {
+            q.push(temp->left);
+        }
+        if (temp->right)
+        {
+            q.push(temp->right);
+        }
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->left->right->left = new Node(8);
+    root->left->right->right = new Node(9);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    root->right->right->right = new Node(10);
+
+    level_order(root);
+    return 0;
+}

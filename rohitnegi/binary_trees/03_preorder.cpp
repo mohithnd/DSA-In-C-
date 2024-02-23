41bf90a1db84531e6b44c8a423c181e844e61ec2
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-02-23 13:26:29 +0530

    Pre, In and Post Order Traversal of a Binary Tree

---
+++ rohitnegi/binary_trees/03_preorder.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
+        this->data = data;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+};
+
+void preorder(Node *root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->left->left = new Node(3);
+    root->left->left->left = new Node(4);
+    root->left->left->right = new Node(5);
+    root->left->right = new Node(6);
+    root->right = new Node(7);
+    root->right->right = new Node(8);
+    root->right->right->left = new Node(9);
+    root->right->right->right = new Node(10);
+
+    preorder(root);
+    return 0;
+}
---
+++ rohitnegi/binary_trees/05_postorder.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
+        this->data = data;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+};
+
+void postorder(Node *root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->left->left = new Node(3);
+    root->left->left->left = new Node(4);
+    root->left->left->right = new Node(5);
+    root->left->right = new Node(6);
+    root->right = new Node(7);
+    root->right->right = new Node(8);
+    root->right->right->left = new Node(9);
+    root->right->right->right = new Node(10);
+
+    postorder(root);
+    return 0;
+}

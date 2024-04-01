c55a024c71802feef9cdebc8246c803bfe1d6b51
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-04-01 22:48:21 +0530

    Removed Unnecessary Function

--- rohitnegi/strings/16_longest_prefix_suffix.cpp
+++ rohitnegi/strings/16_longest_prefix_suffix.cpp
@@ -2,19 +2,6 @@
 #include <vector>
 using namespace std;
 
-string reverse(string s)
-{
-    int i = 0;
-    int j = s.length() - 1;
-    while (i < j)
-    {
-        swap(s[i], s[j]);
-        i++;
-        j--;
-    }
-    return s;
-}
-
 string longest_prefix_suffix_1(string s)
 {
     vector<string> prefs;

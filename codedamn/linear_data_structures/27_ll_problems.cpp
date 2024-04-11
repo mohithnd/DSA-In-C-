74c9e03048f9d0b885c2b6805de2074e5f1fcdfb
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-04-11 11:09:26 +0530

    Reverse A Linked List In K Groups

--- codedamn/linear_data_structures/27_ll_problems.cpp
+++ codedamn/linear_data_structures/27_ll_problems.cpp
@@ -41,14 +41,41 @@ Node *swap_pairs_of_ll(Node *head)
     return bwd;
 }
 
+Node *reverse_list_in_k_groups(Node *head, int k)
+{
+    if (head == nullptr || head->next == nullptr)
+    {
+        return head;
+    }
+    Node *prev = nullptr;
+    Node *curr = head;
+    Node *next = nullptr;
+    int cnt = 0;
+    while (cnt < k && curr)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+        cnt++;
+    }
+    Node *rest_of_the_list = reverse_list_in_k_groups(curr, k);
+    head->next = rest_of_the_list;
+    return prev;
+}
+
 int main()
 {
     Node *head = new Node(1);
     head->next = new Node(2);
     head->next->next = new Node(3);
     head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+    head->next->next->next->next->next = new Node(6);
+    head->next->next->next->next->next->next = new Node(7);
+    head->next->next->next->next->next->next->next = new Node(8);
     print(head);
-    head = swap_pairs_of_ll(head);
+    head = reverse_list_in_k_groups(head, 3);
     print(head);
     return 0;
 }

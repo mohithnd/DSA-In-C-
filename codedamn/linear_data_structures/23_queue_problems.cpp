f0face83269b7fe21235a634c1aaa025ba16c852
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-04-11 19:31:07 +0530

    First Negative Integer in every Window of Size K

--- codedamn/linear_data_structures/23_queue_problems.cpp
+++ codedamn/linear_data_structures/23_queue_problems.cpp
@@ -34,6 +34,38 @@ void reverse_using_recursion(queue<int> &q)
 vector<int> first_negative_integer_in_every_window_of_size_k(vector<int> arr, int k)
 {
     vector<int> ans;
+    queue<int> q;
+    for (int i = 0; i < k - 1; i++)
+    {
+        if (arr[i] < 0)
+        {
+            q.push(i);
+        }
+    }
+    int i = k - 1;
+    while (i < arr.size())
+    {
+        if (arr[i] < 0)
+        {
+            q.push(i);
+        }
+        if (q.empty() == false)
+        {
+            if (q.front() <= (i - k))
+            {
+                q.pop();
+            }
+        }
+        if (q.empty())
+        {
+            ans.push_back(0);
+        }
+        else
+        {
+            ans.push_back(arr[q.front()]);
+        }
+        i++;
+    }
     return ans;
 }
 
